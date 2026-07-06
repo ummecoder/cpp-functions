3b7c404d21aeafae5d3cc7069e3d8443663b7b65
ummecoder <umesabiha572@gmail.com>
2026-07-06 02:18:11 -0700

    add function program

---
+++ function3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// Function
+int square(int n) {
+    return n * n;
+}
+
+int main() {
+    int num = 5;
+    cout << "Square = " << square(num);
+    return 0;
+}
